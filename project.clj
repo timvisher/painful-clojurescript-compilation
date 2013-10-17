be38112ce3640c635755ed534e6c43cee7666941
Tim Visher <tvisher@monetate.com>
2013-10-16 21:43:31 -0400

    Adding output-dir per dnolen and checking in generated core.js. EOM

--- project.clj
+++ project.clj
@@ -8,13 +8,17 @@
   :profiles {:dev {:plugins [[lein-cljsbuild "0.3.4"]]
                    :cljsbuild {:builds [{:source-paths ["src"]
                                          :compiler {:output-to     "painful-clojurescript-compilation-none.js"
-                                                    :optimizations :none}}
+                                                    :optimizations :none
+                                                    :output-dir    "target/none"}}
                                         {:source-paths ["src"]
                                          :compiler {:output-to     "painful-clojurescript-compilation-whitespace.js"
+                                                    :output-dir    "target/whitespace"
                                                     :optimizations :whitespace}}
                                         {:source-paths ["src"]
                                          :compiler {:output-to     "painful-clojurescript-compilation-simple.js"
+                                                    :output-dir    "target/simple"
                                                     :optimizations :simple}}
                                         {:source-paths ["src"]
                                          :compiler {:output-to     "painful-clojurescript-compilation-advanced.js"
+                                                    :output-dir    "target/advanced"
                                                     :optimizations :advanced}}]}}})
